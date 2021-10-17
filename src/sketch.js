b5ea948a4f4b16e0dc467f5e79f4be309096ac3b
Kamyar <kamyarkarimi98@gmail.com>
2021-10-17 16:19:36 -0400

    fractal init

--- src/sketch.js
+++ src/sketch.js
@@ -1,10 +1,37 @@
+var angle = 0;
+
+var slider;
+
 function setup() {
     var canvas = createCanvas(500, 500);
     canvas.parent("#centerme");
+    slider = createSlider(0, TWO_PI, PI / 4, 0.01);
+    slider.parent("#centerme");
 }
 
 function draw() {
-    background(127);
-    rect(100,100,100,100);
-    rect(300,100,100,100);
+    background(51);
+    angle = slider.value();
+    stroke(255);
+    translate(250, height);
+    branch(100);
+
+}
+
+function branch(len) {
+
+    line(0, 0, 0, -len);
+    translate(0, -len);
+    // rotate(angle);
+    if (len > 4)
+    {
+        push();
+        rotate(angle);
+        branch( len * 0.67 );
+        pop();
+        push();
+        rotate(-angle);
+        branch(len * 0.67);
+        pop();
+    }    
 }
