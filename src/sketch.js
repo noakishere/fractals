821710968bc086ecfb3f291da61620521ab26dc4
Kamyar <kamyarkarimi98@gmail.com>
2021-10-17 15:42:22 -0400

    init

---
+++ src/sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+    var canvas = createCanvas(500, 500);
+    canvas.parent("#centerme");
+}
+
+function draw() {
+    background(127);
+    rect(100,100,100,100);
+    rect(300,100,100,100);
+}
